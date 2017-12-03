002f2a338854df56fa511a5d72b7775b6f88bb84
kayazekioglu <kayazekioglu@gmail.com>
2017-12-03 12:04:12 -0800

    Adding another basset and upgrading the main page to PDC.

--- biscuit.js
+++ biscuit.js
@@ -1,121 +1,79 @@
-
 // two variables the example used, I don't think we need them. TODO findout if we need them
-var container, stats;
 
-var camera, scene, renderer;
-
-var mouseX = 0, mouseY = 0;
-
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
-
+var scene;
 
 init();
-animate();
-
 
 function init() {
+    // create the renderer
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
-    container = document.createElement( 'div' );
-    document.body.appendChild( container );
-
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+    // and the camera
+    var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
 
     // How far away the camera is from our biscuit
-    camera.position.z = 750;
-
-    // scene
-
-    scene = new THREE.Scene();
-
-    var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
-    scene.add( ambientLight );
+    camera.position.set(0, 0, 750);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     // The first argument to THREE.PointLight appears to be the color that the biscuit is illuminated with.
     var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
     camera.add( pointLight );
-    scene.add( camera );
 
-    // model
+    scene = new THREE.Scene();
+    var ambientLight = new THREE.AmbientLight( 0xcccccc, .7 );
+    scene.add( ambientLight );
 
-    var onProgress = function ( xhr ) {
-        if ( xhr.lengthComputable ) {
-            var percentComplete = xhr.loaded / xhr.total * 100;
-            console.log( Math.round(percentComplete, 2) + '% downloaded' );
-        }
-    };
+    function newBasset(name, copies, folder, mtl_file, obj_file, scale) {
+        return new Promise(function(resolve, reject) {
+            var mtlLoader = new THREE.MTLLoader();
+            mtlLoader.setPath(folder);
+            mtlLoader.load(mtl_file, function( materials ) {
+                materials.preload();
+                var objLoader = new THREE.OBJLoader();
+                objLoader.setMaterials( materials );
+                objLoader.setPath(folder);
+                objLoader.load(obj_file, function ( object ) {
+                    object.position.set(0, 0, 0);
+                    object.name = name;
+                    object.children[0].scale.set(scale, scale, scale);
+                    scene.add( object );
+                    Array(copies).fill().map(function () {
+                        scene.add(object.clone());
+                    })
+                    resolve();
+                });
+            });
+        })
+    }
 
-    var onError = function ( xhr ) { };
+    promises = []
+    promises.push(newBasset(
+        'bisc1',
+        0,
+        'bisc/',
+        '59f116fab83ffbdbfeebb2fc_5a1bb2856bbc6105812c19aa_100lod.mtl',
+        '59f116fab83ffbdbfeebb2fc_5a1bb2856bbc6105812c19a9_100lod.obj',
+        1));
+    promises.push(newBasset(
+        'bisc2',
+        0,
+        'bisc/',
+        '5a0104defbcb3b049f2022ae_5a0104defbcb3b049f2022b1_100lod.mtl',
+        '5a0104defbcb3b049f2022ae_5a0104defbcb3b049f2022b0_100lod.obj',
+        600));
 
-    THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
+    Promise.all(promises).then(
+            function () {
+                console.log(scene.children);
+                renderer.render(scene, camera);
 
-    var mtlLoader = new THREE.MTLLoader();
-    mtlLoader.setPath( 'bisc/' );
-    mtlLoader.load( '59f116fab83ffbdbfeebb2fc_5a1bb2856bbc6105812c19aa_100lod.mtl', function( materials ) {
-
-        materials.preload();
-
-        var objLoader = new THREE.OBJLoader();
-        objLoader.setMaterials( materials );
-        objLoader.setPath( 'bisc/' );
-        objLoader.load( '59f116fab83ffbdbfeebb2fc_5a1bb2856bbc6105812c19a9_100lod.obj', function ( object ) {
-
-            object.position.y = - 95;
-            scene.add( object );
-
-        }, onProgress, onError );
-
-    });
-
-    //
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    container.appendChild( renderer.domElement );
-
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-
-    //
-
-    window.addEventListener( 'resize', onWindowResize, false );
-
+                // initialise controls
+                var controls = new THREE.PointDragControls();
+                controls.init( scene,camera,renderer, {
+                    auto_render: true
+                });
+            }
+    );
 }
-
-function onWindowResize() {
-
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
-function onDocumentMouseMove( event ) {
-
-    mouseX = ( event.clientX - windowHalfX ) / 2;
-    mouseY = ( event.clientY - windowHalfY ) / 2;
-
-}
-
-function animate() {
-
-    requestAnimationFrame( animate );
-    render();
-
-}
-
-function render() {
-
-    camera.position.x += ( mouseX - camera.position.x ) * .05;
-    camera.position.y += ( - mouseY - camera.position.y ) * .05;
-
-    camera.lookAt( scene.position );
-
-    renderer.render( scene, camera );
-
-}
-
